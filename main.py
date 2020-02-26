f470133b008314da4c5c1f8516236a8cf7cf755e
Zaitsev Georgiy <57594615+orijuke@users.noreply.github.com>
2020-02-26 18:32:19 +0500

    Update moving

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import requests
 import os
 import sys
-
+from math import sqrt
 import pygame
 
-size = (600, 450)
-zoom = 12
+
 
 
 def get_boundary(geo_object):
@@ -33,7 +32,13 @@ def get_coords(geo_object):
 
 # Инициализируем pygame
 pygame.init()
+size = (600, 450)
+screen = pygame.display.set_mode(size)
 position = get_coords('Москва')
+xy = [float(position.split(',')[0]), float(position.split(',')[1])]
+
+zoom = 12
+k = 1 / (2 ** zoom)
 does = True
 updated = False
 while does:
@@ -47,22 +52,34 @@ while does:
             if event.key == pygame.K_PAGEUP:
                 if zoom != 17:
                     zoom += 1
+            k = 1 / (2 ** zoom)
+
             if event.key == pygame.K_UP:
-                xy[0] += 0.5
+                xy[1] += k * size[1] / sqrt(2)
+                if xy[1] > 90:
+                    xy[1] -= k * size[1] / sqrt(2)
                 position = str(xy[0]) + ',' + str(xy[1])
             if event.key == pygame.K_DOWN:
-                xy[0] -= 0.5
+                xy[1] -= k * size[1] / sqrt(2)
+                if xy[1] < -90:
+                    xy[1] += k * size[1] / sqrt(2)
                 position = str(xy[0]) + ',' + str(xy[1])
+
             if event.key == pygame.K_LEFT:
-                xy[1] -= 0.5
+                xy[0] -= k * size[0] * sqrt(2)
+                if xy[0] < -180:
+                    xy[0] += k * size[0] * sqrt(2)
                 position = str(xy[0]) + ',' + str(xy[1])
             if event.key == pygame.K_RIGHT:
-                xy[1] += 0.5
+                xy[0] += k * size[0] * sqrt(2)
+                if xy[0] > 180:
+                    xy[0] -= k * size[0] * sqrt(2)
                 position = str(xy[0]) + ',' + str(xy[1])
             print(xy)
             updated = False
+
     if not updated:
-        xy = [float(position.split(',')[0]), float(position.split(',')[1])]
+
         response = None
         map_request = "https://static-maps.yandex.ru/1.x/?ll=" + position + f"&z={zoom}&size=600,450&l=map"
         response = requests.get(map_request)
@@ -78,7 +95,7 @@ while does:
         with open(map_file, "wb") as file:
             file.write(response.content)
         updated = True
-    screen = pygame.display.set_mode((600, 450))
+
     screen.blit(pygame.image.load(map_file), (0, 0))
     pygame.display.flip()
 pygame.quit()
