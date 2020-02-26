9ba80bf4f76849a1ee7390e3204f0be5770a7a91
Zaitsev Georgiy <57594615+orijuke@users.noreply.github.com>
2020-02-26 17:43:41 +0500

    Add zooming

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def get_boundary(geo_object):
         envelop = GeoObject['boundedBy']['Envelope']
         return envelop['lowerCorner'].replace(' ', ',') + '~' + envelop['upperCorner'].replace(' ', ',')
 
+
 def get_coords(geo_object):
     api_adress = 'https://geocode-maps.yandex.ru/1.x'
     apikey = '40d1649f-0493-4b70-98ba-98533de7710b'
@@ -29,31 +30,39 @@ def get_coords(geo_object):
         return ','.join(GeoObject['Point']['pos'].split(' '))
 
 
-moscow = get_coords('Москва')
-
-
-response = None
-map_request = "https://static-maps.yandex.ru/1.x/?ll=" + moscow + f"&z={zoom}&size=600,450&l=map"
-response = requests.get(map_request)
-
-if not response:
-    print("Ошибка выполнения запроса:")
-    print(map_request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
-
-# Запишем полученное изображение в файл.
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
-
 # Инициализируем pygame
 pygame.init()
-screen = pygame.display.set_mode((600, 450))
-# Рисуем картинку, загружаемую из только что созданного файла.
-screen.blit(pygame.image.load(map_file), (0, 0))
-# Переключаем экран и ждем закрытия окна.
-pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
+
+does = True
+updated = False
+while does:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            does = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_PAGEDOWN:
+                zoom -= 1
+            if event.key == pygame.K_PAGEUP:
+                zoom += 1
+            updated = False
+    if not updated:
+        position = get_coords('Москва')
+        response = None
+        map_request = "https://static-maps.yandex.ru/1.x/?ll=" + position + f"&z={zoom}&size=600,450&l=map"
+        response = requests.get(map_request)
+
+        if not response:
+            print("Ошибка выполнения запроса:")
+            print(map_request)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+            sys.exit(1)
+
+        # Запишем полученное изображение в файл.
+        map_file = "map.png"
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+        updated = True
+    screen = pygame.display.set_mode((600, 450))
+    screen.blit(pygame.image.load(map_file), (0, 0))
+    pygame.display.flip()
 pygame.quit()
